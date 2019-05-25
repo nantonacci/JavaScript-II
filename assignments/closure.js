dd2662dbc5c07a1d859d87afdf1bdd476538e194
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-05-25 11:16:37 -0700

    Closure stretch done.

--- assignments/closure.js
+++ assignments/closure.js
@@ -17,7 +17,15 @@ helloWorld('Nico');
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return () => {
+    return ++count;
+  };
 };
+
+const newCounter = counter();
+console.log(newCounter());
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -27,4 +35,16 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  let obj = {
+    increment() {
+      return ++count;
+    },
+    decrement() {
+      return --count;
+    }
+  };
+  return obj;
 };
+
+console.log(counterFactory());
