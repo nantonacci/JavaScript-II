8d60a726b145c01fb44c3d90b966d2b41e72b219
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-05-25 10:10:37 -0700

    Completed simple closure challenge.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,9 +1,18 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function helloWorld(name) {
+  let greeting = 'Hello';
+
+  let speak = function() {
+    console.log(`${greeting}, ${name}!`);
+  };
+  return speak();
+}
+
+helloWorld('Nico');
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
